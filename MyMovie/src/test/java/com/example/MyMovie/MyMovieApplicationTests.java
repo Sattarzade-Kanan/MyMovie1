541ba514601371a6c50696b0c7fdccd776151775
Sattarzade-Kanan <chinkan1980@gmail.com>
2025-12-05 12:35:18 +0400

    Start new Project

---
+++ MyMovie/src/test/java/com/example/MyMovie/MyMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyMovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
